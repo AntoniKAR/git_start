6e92185d169e14c448e91e933dce34a7697fb289
PC <toha.karto@yandex.ru>
2022-01-29 00:25:41 +0300

    Added Anton_kar.py and .gitignore

---
+++ Anton_kar.py
@@ -0,0 +1,6 @@
+print("Good evening dear members of Otus club!")
+# Сделали пустой репозиторий
+print("Добавили строку print")
+print("Регмаглиптовый")
+# Создали репозиторий
+print("Создаем репозиторий")
