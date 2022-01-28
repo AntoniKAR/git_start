64fc21286694aac469d9f21070227ab9dbb84244
PC <toha.karto@yandex.ru>
2022-01-29 01:34:18 +0300

    Show how does rebase work

--- Anton_kar.py
+++ Anton_kar.py
@@ -4,4 +4,5 @@ print("Добавили строку print")
 print("Регмаглиптовый")
 # Создали репозиторий
 print("Создаем репозиторий")
-print("From dev branch") # From develop brach
+print("From dev brancgit h") # From develop brach
+print("Being Changed in order to show how does "git rebase" work")
